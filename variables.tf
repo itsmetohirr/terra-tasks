79f0ff97bc1002989fbd33ed5f13546049ee4be9
Tohir <itsmetohir@gmail.com>
2025-10-22 18:20:51 +0500

    data done

--- variables.tf
+++ variables.tf
@@ -1,17 +1,17 @@
 variable "state_bucket" {
   description = "Name of the S3 bucket storing the remote Terraform state"
   type        = string
-  default = "cmtr-o84gfl9h-tf-state-1761138286"
+  default     = "cmtr-o84gfl9h-tf-state-1761138286"
 }
 
 variable "state_key" {
   description = "Path/key of the remote state file within the S3 bucket"
   type        = string
-  default = "infra.tfstate"
+  default     = "infra.tfstate"
 }
 
 variable "aws_region" {
   description = "AWS region where the S3 bucket is located"
   type        = string
-  default = "us-east-1"
+  default     = "us-east-1"
 }
