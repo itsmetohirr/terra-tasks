2914caece8768fddc990d140db3634ef31993fe7
Tohir <itsmetohir@gmail.com>
2025-10-22 13:21:48 +0500

    done

--- main.tf
+++ main.tf
@@ -1,7 +1,3 @@
-terraform {
-  required_version = var.tf_version
-}
-
 provider "aws" {
   region = "us-east-1"
 }
--- variables.tf
+++ variables.tf
@@ -3,9 +3,3 @@ variable "vpc_cidr" {
   type        = string
   default     = "10.10.0.0/16"
 }
-
-variable "tf_version" {
-  description = "tf version"
-  type = string
-  default = ">= 1.5.7"
-}
