36531886f6dbe0f5d33f53626907acb5cc1c1f23
Tohir <itsmetohir@gmail.com>
2025-10-22 18:20:03 +0500

    data done

--- variables.tf
+++ variables.tf
@@ -1,5 +1,17 @@
-variable "vpc_cidr" {
-  description = "vpc cidr"
+variable "state_bucket" {
+  description = "Name of the S3 bucket storing the remote Terraform state"
   type        = string
-  default     = "10.10.0.0/16"
+  default = "cmtr-o84gfl9h-tf-state-1761138286"
+}
+
+variable "state_key" {
+  description = "Path/key of the remote state file within the S3 bucket"
+  type        = string
+  default = "infra.tfstate"
+}
+
+variable "aws_region" {
+  description = "AWS region where the S3 bucket is located"
+  type        = string
+  default = "us-east-1"
 }
