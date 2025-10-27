4f0578a3d77d62e00e4dbca8472bf05369c5353a
Tohir <itsmetohir@gmail.com>
2025-10-27 16:30:17 +0500

    final done

--- modules/application/main.tf
+++ modules/application/main.tf
@@ -100,3 +100,18 @@ resource "aws_lb_target_group" "tg" {
   }
 }
 
+resource "aws_lb_listener" "simple_http" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
+}
+
+resource "aws_autoscaling_attachment" "asg_tg_attachment" {
+  autoscaling_group_name = aws_autoscaling_group.asg.name
+  lb_target_group_arn   = aws_lb_target_group.tg.arn
+}
